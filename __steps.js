a06ba66a3c75880fb3bba5f43bc11038d1c6e5a9
unknown <asha.engrbd@gmail.com>
2023-03-20 18:11:08 +0000

    connect database and hardcoded data add inside of database from node-mongo-crud-server

--- __steps.js
+++ __steps.js
@@ -3,4 +3,5 @@
  * 1. create a project for server
  * 2. install express, cors, mongodb 
  * 3. simple server set
+ * 4. 
  */
--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middlewire
+app.use(cors())
+app.use(express.json())
+
+// user:dbUser2
+// password: nVjdXJyyiLQpGEBx
+
+
+const uri = "mongodb+srv://dbUser2:nVjdXJyyiLQpGEBx@cluster0.njs77my.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+        const user = {
+            name: 'testing test',
+            email: 'testing@gmail.com'
+        }
+        const result = await userCollection.insertOne(user);
+        console.log(result);
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.log(err));
+
+
+
 app.get('/', (req, res) =>{
-    res.send('node server is running');
+    res.send('Hello from node mongo crud server');
 })
 
 app.listen(port, () =>{
-    console.log(`simple node server running in port ${port}`);
+    console.log(`Listening to port ${port}`);
 })
