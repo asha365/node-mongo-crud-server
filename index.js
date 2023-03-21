6df7b4091f517120fdedc06c843dcb89b1b27ebb
unknown <asha.engrbd@gmail.com>
2023-03-21 09:39:30 +0000

    data read database to server

--- index.js
+++ index.js
@@ -11,27 +11,32 @@ app.use(express.json())
 // user:dbUser2
 // password: nVjdXJyyiLQpGEBx
 
-
 const uri = "mongodb+srv://dbUser2:nVjdXJyyiLQpGEBx@cluster0.njs77my.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try{
         const userCollection = client.db('nodeMongoCrud').collection('users');
-        const user = {
-            name: 'testing test',
-            email: 'testing@gmail.com'
-        }
-        const result = await userCollection.insertOne(user);
-        console.log(result);
+
+        app.get('/users', async (req, res) =>{
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        app.post('/users', async (req, res) =>{
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user); 
+            res.send(result);
+        });
     }
     finally{
 
     }
 }
 run().catch(err => console.log(err));
-
-
 
 app.get('/', (req, res) =>{
     res.send('Hello from node mongo crud server');
